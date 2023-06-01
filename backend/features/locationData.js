a283de6cc9d9850cbce4799b9fb66899f4fcc553
Sydney Sisco <hello@sydsis.co>
2023-05-31 21:47:23 -0700

    vastly reduce the number of ws messages

--- backend/features/locationData.js
+++ backend/features/locationData.js
@@ -2,41 +2,49 @@ let locationData = null;
 let timeout = null;
 
 module.exports = (io) => {
-  console.log('locationData.js module loaded.')
+  console.log('locationData.js module loaded.');
 
+  let interval;
 
+  const sendLocationUpdate = () => {
+    if (locationData) {
+      io.emit('locationUpdate', locationData);
+    }
+  };
 
   io.on('connection', (socket) => {
-    // emit initial location data (could be null)
-    socket.emit('locationUpdate', locationData);
+    // Emit initial location data if available
+    sendLocationUpdate();
 
-    // someone is sharing their location
+    // Start interval if not already started
+    if (!interval) {
+      interval = setInterval(sendLocationUpdate, 30 * 1000);
+    }
+
+    // Someone is sharing their location
     socket.on('locationShare', (data) => {
       console.log('locationShare event. Data:', data);
 
-      // save the location data with a timestamp
+      // Save the location data with a timestamp
       locationData = {
         ...data,
         timestamp: Date.now(),
       };
 
-      // clear the timeout if it exists
+      // Clear the timeout if it exists
       if (timeout) {
         clearTimeout(timeout);
       }
-      // set a timeout to clear the location data after 10 minutes
+      // Set a timeout to clear the location data after 10 minutes
       timeout = setTimeout(() => clearLocationData(io), 10 * 60 * 1000);
+    });
 
-      io.emit('locationUpdate', data);
+    // Clear interval on disconnect if no clients connected
+    socket.on('disconnect', () => {
+      if (io.engine.clientsCount === 0) {
+        clearInterval(interval);
+        interval = null;
+      }
     });
   });
-}
-
-// a function that clears the location data if it's older than 10 minutes
-const clearLocationData = (io) => {
-  console.log('clearing location data. Timestamp:', locationData.timestamp, 'Now:', Date.now());
-
-  locationData = null;
-
-  io.emit('locationUpdate', locationData);
 };
