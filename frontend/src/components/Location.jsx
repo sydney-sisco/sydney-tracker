b81be5cb5bcd7f4ce421dd33b02003708f7fa106
Sydney Sisco <hello@sydsis.co>
2023-06-01 08:04:58 -0700

    add an untested wakelock feature to keep the screen on while tracking is enabled

--- frontend/src/components/Location.jsx
+++ frontend/src/components/Location.jsx
@@ -6,9 +6,39 @@ const Location = () => {
   const trackMeQueryParam = getUrlParameter('trackme');
   const [shouldTrack, setShouldTrack] = useState(trackMeQueryParam === 'yes');
   const { location, error } = useGeolocation(shouldTrack);
+  const [wakeLock, setWakeLock] = useState(null);
+
+  async function requestWakeLock() {
+    if ('wakeLock' in navigator) {
+      try {
+        const newWakeLock = await navigator.wakeLock.request('screen');
+        setWakeLock(newWakeLock);
+        console.log('Screen wake lock acquired:', newWakeLock);
+      } catch (err) {
+        console.error(`Failed to acquire screen wake lock: ${err.name}`, err);
+      }
+    } else {
+      console.error('Wake Lock API not supported.');
+    }
+  }
+
+  async function releaseWakeLock() {
+    if (wakeLock !== null) {
+      try {
+        await wakeLock.release();
+        console.log('Screen wake lock released:', wakeLock);
+        setWakeLock(null);
+      } catch (err) {
+        console.error(`Failed to release screen wake lock: ${err.name}`, err);
+      }
+    }
+  }
 
   useEffect(() => {
     setShouldTrack(trackMeQueryParam === 'yes');
+    if(trackMeQueryParam === 'yes') {
+      requestWakeLock();
+    }
   }, [trackMeQueryParam]);
 
   if (error) {
@@ -25,6 +55,8 @@ const Location = () => {
       <h3>User Location</h3>
       <p>Latitude: {latitude}</p>
       <p>Longitude: {longitude}</p>
+      <p>Tracking: {shouldTrack ? 'Yes' : 'No'}</p>
+      <p>Wake Lock: {wakeLock ? 'Yes' : 'No'}</p>
     </div>
   );
 };
