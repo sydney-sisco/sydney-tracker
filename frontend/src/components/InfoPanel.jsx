8d8c18c6f385e20860f9bcb087a019a359aef763
Sydney Sisco <hello@sydsis.co>
2023-06-27 21:09:52 -0700

    add speed and activity to info panel. A bit of CSS

--- frontend/src/components/InfoPanel.jsx
+++ frontend/src/components/InfoPanel.jsx
@@ -1,11 +1,12 @@
+import BackgroundEffect from './BackgroundEffect';
 import styles from './InfoPanel.module.css';
-import avatar from '/person-group-svgrepo-com.svg'
+import avatar from '/person-svgrepo-com.svg'
 
 function InfoPanel({ center }) {
   console.log('center', center);
   return (
     <div className={styles.infoPanel}>
-      <h1>Tracking</h1>
+      <h1>Subject</h1>
       <div className={styles.avatar}>
         <div className={styles.avatarImage}>
           <img src={avatar} alt="avatar" />
@@ -17,6 +18,14 @@ function InfoPanel({ center }) {
       <div className={styles.location}>
         <h3>Location</h3>
         <p>{center.lat.toFixed(5)}, {center.lng.toFixed(5)}</p>
+      </div>
+      <div className={styles.speed}>
+        <h3>Speed</h3>
+        <p>0.0 km/h</p>
+      </div>
+      <div className={styles.activity}>
+        <h3>Activity</h3>
+        <p>Unknown</p>
       </div>
     </div>
   );
