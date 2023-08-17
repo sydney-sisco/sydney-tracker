874fe61cbc35996f0753d74f92eda1f0d1eb99e8
Sydney Sisco <hello@sydsis.co>
2023-08-16 22:53:22 -0700

    add some yummy debugging statements to the Location info to figure out why wakelock is suddenly not working

--- frontend/src/components/Location.jsx
+++ frontend/src/components/Location.jsx
@@ -11,18 +11,23 @@ const Location = () => {
   const [shouldTrack, setShouldTrack] = useState(trackMeQueryParam === 'yes');
   const { location, error } = useGeolocation(shouldTrack);
   const [wakeLock, setWakeLock] = useState(null);
+  const [errorWakeLock, setErrorWakeLock] = useState(null);
 
   async function requestWakeLock() {
+    setErrorWakeLock('requestWakeLock')
     if ('wakeLock' in navigator) {
       try {
         const newWakeLock = await navigator.wakeLock.request('screen');
         setWakeLock(newWakeLock);
+        setErrorWakeLock(null);
         console.log('Screen wake lock acquired:', newWakeLock);
       } catch (err) {
+        setErrorWakeLock(err);
         console.error(`Failed to acquire screen wake lock: ${err.name}`, err);
       }
     } else {
       console.error('Wake Lock API not supported.');
+      setErrorWakeLock('Wake Lock API not supported.');
     }
   }
 
@@ -41,9 +46,12 @@ const Location = () => {
   useEffect(() => {
     // Function to reacquire wake lock when the page is visible
     const handleVisibilityChange = async () => {
+      setErrorWakeLock('handleVisibilityChange')
       if (document.visibilityState === 'visible') {
+        setErrorWakeLock('requesting')
         await requestWakeLock();
       } else {
+        setErrorWakeLock('requesting else')
         setWakeLock(null);
       }
     };
@@ -60,23 +68,27 @@ const Location = () => {
   useEffect(() => {
     setShouldTrack(trackMeQueryParam === 'yes');
     if(trackMeQueryParam === 'yes') {
+      setErrorWakeLock('query param yes')
       requestWakeLock();
+    } else {
+      setErrorWakeLock('query param no')
     }
   }, [trackMeQueryParam]);
 
-  if (!location) {
+  if (!shouldTrack) {
     return <div> </div>;
   }
 
-  const { latitude, longitude } = location;
+  // const { latitude, longitude } = location;
   return (
     <div className={styles.locationContainer}>
       <div>
         <h3>User Location</h3>
-        <p>Latitude: {latitude}</p>
-        <p>Longitude: {longitude}</p>
+        <p>Latitude: {location?.latitude}</p>
+        <p>Longitude: {location?.longitude}</p>
         <p>Tracking: {shouldTrack ? 'Yes' : 'No'}</p>
         <p>Wake Lock: {wakeLock ? 'Yes' : 'No'}</p>
+        <p>Wake Lock Error: {errorWakeLock}</p>
         {error && <p>Error: {error}</p>}
       </div>
       <ActivityComponent />
